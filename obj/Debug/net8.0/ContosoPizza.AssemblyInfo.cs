0410681e8e0a005c6e71039fa1e42cc4741aad17
Ikenna Benedict Nwaka <inwaka39@gmail.com>
2025-10-01 19:32:28 +0200

    BuiltWebApi

--- obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90ff3573b11b77f9b897c01e3e3b36d3d2d68fc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57b2e3b1f9e5d49dbd4ec03c6a486767e22f5738")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
